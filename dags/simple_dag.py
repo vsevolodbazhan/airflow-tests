88928522e1f571007b284a878ac387cbded8f2ca
Vsevolod Mycroft <mycroft@deeplay.io>
2021-03-11 18:39:50 +1000

    Rewrite `simple_dag`

--- dags/simple_dag.py
+++ dags/simple_dag.py
@@ -1,21 +1,34 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
 from airflow.utils.dates import days_ago
-
 
 with DAG(
     dag_id="simple_dag",
     description="Simple DAG for experimenting with tests.",
     start_date=days_ago(n=1),
 ) as dag:
-    echo_execution_date = BashOperator(
-        task_id="echo_execution_date",
-        bash_command="echo {{ execution_date }}",
+    start_task = DummyOperator(
+        task_id="start_task",
     )
 
-    echo_next_ds = BashOperator(
-        task_id="echo_next_ds",
-        bash_command="echo {{ next_ds }}",
+    first_task = BashOperator(
+        task_id="first_task",
+        bash_command="echo 1",
     )
 
-    echo_execution_date >> echo_next_ds
+    second_task = BashOperator(
+        task_id="second_task",
+        bash_command="echo 2",
+    )
+
+    third_task = BashOperator(
+        task_id="third_task",
+        bash_command="echo 3",
+    )
+
+    end_task = DummyOperator(
+        task_id="end_task",
+    )
+
+    start_task >> first_task >> [second_task, third_task] >> end_task
