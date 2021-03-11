94e046adf096656778e2b0ccd88f6b06876b4297
Vsevolod Mycroft <mycroft@deeplay.io>
2021-03-11 16:48:29 +1000

    Add task running helper methods

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,8 @@
-from typing import Dict
+from typing import Any, Dict
 
+import pendulum
 import pytest
-from airflow.models import DAG
+from airflow.models import DAG, TaskInstance
 from airflow.models.dagbag import DagBag
 from airflow.models.baseoperator import BaseOperator
 
@@ -15,6 +16,15 @@ def get_dag(dag_id: str) -> DAG:
 
 
 @pytest.helpers.register
+def get_dag_structure(dag: DAG) -> Dict:
+    structure = {}
+    for task in dag.tasks:
+        task_id, downstream_task_ids = task.task_id, list(task.downstream_task_ids)
+        structure[task_id] = downstream_task_ids
+    return structure
+
+
+@pytest.helpers.register
 def get_task(dag: DAG, task_id: str) -> BaseOperator:
     for task in dag.tasks:
         if task.task_id == task_id:
@@ -23,9 +33,27 @@ def get_task(dag: DAG, task_id: str) -> BaseOperator:
 
 
 @pytest.helpers.register
-def get_dag_structure(dag: DAG) -> Dict:
-    structure = {}
-    for task in dag.tasks:
-        task_id, downstream_task_ids = task.task_id, list(task.downstream_task_ids)
-        structure[task_id] = downstream_task_ids
-    return structure
+def run_task(task_instance: TaskInstance) -> None:
+    task_instance.run(test_mode=True)
+
+
+@pytest.helpers.register
+def run_xcom_task(task_instance: TaskInstance, delete_xcom_entry: bool = True) -> Any:
+    run_task(task_instance=task_instance)
+    value = task_instance.xcom_pull(task_ids=[task_instance.task_id])
+    if delete_xcom_entry is True:
+        task_instance.clear_xcom_data()
+    return value
+
+
+@pytest.helpers.register
+def get_task_instance(
+    task: BaseOperator,
+    execution_date: pendulum.datetime,
+) -> TaskInstance:
+    return TaskInstance(task=task, execution_date=execution_date)
+
+
+@pytest.helpers.register
+def get_xcom_value(task_instance: TaskInstance):
+    return task_instance.xcom_pull(task_ids=[task_instance.task_id])
